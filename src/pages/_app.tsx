46707f0fd31f15c0fd54ceed0579b64d157ca5f8
Artur Tavares <artur@loba.pt>
2021-02-27 14:16:59 +0000

    5ª Aula

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,7 @@ import { ChallengesProvider } from '../contexts/ChallengeContext';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
